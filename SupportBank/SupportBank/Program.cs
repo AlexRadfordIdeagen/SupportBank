5598d118630bc83359fb57c9e6765717ba3161fd
IDEAGENPLC\Alex.Radford <alex.radford@ideagen.com>
2018-04-04 13:36:12 +0100

    Added files and the implementation of part 1 yet error occuring with converting to a double

---
+++ SupportBank/SupportBank/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SupportBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<Transaction> transactions = new List<Transaction>();
+
+            using (var reader = new StreamReader(@"C:\Users\Alex.Radford\source\Training\SupportBank\Transactions2014.csv"))
+            {
+
+
+                while (!reader.EndOfStream)
+                {
+                    var splits = reader.ReadLine().Split(',');
+                    Transaction myTranscation = new Transaction(splits[0],splits[1],splits[2],splits[3],splits[4]);
+                    transactions.Add(myTranscation);
+
+                }
+            }
+            Console.WriteLine("Enter your command");
+            string command = Console.ReadLine().ToLower();
+            
+            if (command == "list all")
+            {
+                var dic = AggregatePerEntry(transactions);
+                PrintAggregation(dic);
+            }
+
+          
+            Console.ReadLine();
+        }
+
+        private static void PrintAggregation(Dictionary<string, double> dic)
+        {
+            foreach (var item in dic)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static Dictionary<string, double> AggregatePerEntry(List<Transaction> transactions)
+        {
+            var dictionary = new Dictionary<string, double>();
+
+            foreach (var transaction in transactions)
+            {
+                if(dictionary.ContainsKey(transaction.To))
+                {
+                    dictionary[transaction.To] += transaction.Amount;
+                }
+                if (dictionary.ContainsKey(transaction.From))
+                {
+                    dictionary[transaction.To] -= transaction.Amount;
+                }
+
+
+
+                else
+                    dictionary.Add(transaction.To, transaction.Amount);
+               
+            }
+            return dictionary;
+        }
+    }
+}
+
+
---
+++ SupportBank/SupportBank/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupportBank
+{
+    class Transaction
+    {
+        public string Date { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Narrative { get; set; }
+        public double Amount { get; set; }
+         
+
+
+        public Transaction(string Date, string From, string To, string Narrative, string Amount)
+        {
+            this.Date = Date;
+            this.From = From;
+            this.To = To;
+            this.Narrative = Narrative;
+            this.Amount = Convert.ToDouble(Amount);
+            
+        }
+    }
+    
+
+}
